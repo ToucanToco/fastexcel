e6e095fb9182a6afc25ffeb68818ab7f1ed5a48b
Luka Peschke <luka.peschke@toucantoco.com>
2022-10-11 17:39:37 +0200

    refactor: Introduced a python-side ExcelFile class
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- python/fastexcel/__init__.py
+++ python/fastexcel/__init__.py
@@ -1,6 +1,8 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, NamedTuple
 
 if TYPE_CHECKING:
+    from typing import Generator
+
     import pandas as pd
 
     from .fastexcel import ExcelSheet
@@ -8,6 +10,16 @@ if TYPE_CHECKING:
 import pyarrow as pa
 
 from .fastexcel import read_excel  # noqa
+
+
+class ExcelFile(NamedTuple):
+    sheet_names: list[str]
+    sheets: "Generator[ExcelSheet, None, None]"
+
+
+def load_excel(path: str) -> ExcelFile:
+    sheet_names, sheets = read_excel(path)
+    return ExcelFile(sheet_names, sheets)
 
 
 def sheet_to_dataframe(s: "ExcelSheet") -> "pd.DataFrame":
