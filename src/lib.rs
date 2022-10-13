fee6587a9a11cd514d284f57d184a03c7a6a89c3
Luka Peschke <luka.peschke@toucantoco.com>
2022-10-13 17:25:51 +0200

    refactor(packaging): Use Cargo.toml version only
    closes #17
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,23 @@ fn read_excel(path: &str) -> Result<ExcelReader> {
     Ok(ExcelReader::try_from_path(path).unwrap())
 }
 
+// Taken from pydantic-core:
+// https://github.com/pydantic/pydantic-core/blob/main/src/lib.rs#L24
+fn get_version() -> String {
+    let version = env!("CARGO_PKG_VERSION").to_string();
+    // cargo uses "1.0-alpha1" etc. while python uses "1.0.0a1", this is not full compatibility,
+    // but it's good enough for now
+    // see https://docs.rs/semver/1.0.9/semver/struct.Version.html#method.parse for rust spec
+    // see https://peps.python.org/pep-0440/ for python spec
+    // it seems the dot after "alpha/beta" e.g. "-alpha.1" is not necessary, hence why this works
+    version.replace("-alpha", "a").replace("-beta", "b")
+}
+
 #[pymodule]
 fn _fastexcel(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(read_excel, m)?)?;
     m.add_class::<ExcelSheet>()?;
     m.add_class::<ExcelReader>()?;
+    m.add("__version__", get_version())?;
     Ok(())
 }
