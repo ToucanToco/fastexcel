bceaa2f9845b835c70284c8a74ba77b647d95daf
Luka Peschke <luka.peschke@toucantoco.com>
2023-06-08 16:37:55 +0200

    feat: Add support for empty sheets
    closes #111
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

---
+++ python/tests/test_empty.py
@@ -0,0 +1,13 @@
+import fastexcel
+import pytest
+
+from utils import path_for_fixture
+
+
+@pytest.mark.parametrize("path", ("empty.ods", "empty.xlsx"))
+def test_empty(path: str) -> None:
+    excel_reader = fastexcel.read_excel(path_for_fixture(path))
+    sheet = excel_reader.load_sheet_by_idx(0)
+
+    assert sheet.to_pandas().empty
+    assert sheet.to_polars().is_empty()
