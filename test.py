6bf5fb152fc132fcfb51ca15afe6ad263c0148e9
Luka Peschke <luka.peschke@toucantoco.com>
2024-03-04 22:20:26 +0100

    add iterations to test.py
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ def get_args() -> argparse.Namespace:
     parser.add_argument(
         "--eager", action="store_true", help="wether the sheet should be loaded eagerly"
     )
+    parser.add_argument(
+        "-i", "--iterations", type=int, help="the number of iterations to do", default=1
+    )
+
     return parser.parse_args()
 
 
@@ -18,11 +22,12 @@ def main():
     excel_file = fastexcel.read_excel(args.file)
     use_columns = args.column or None
 
-    for sheet_name in excel_file.sheet_names:
-        if args.eager:
-            excel_file.load_sheet_eager(sheet_name, use_columns=use_columns)
-        else:
-            excel_file.load_sheet(sheet_name, use_columns=use_columns).to_arrow()
+    for _ in range(args.iterations):
+        for sheet_name in excel_file.sheet_names:
+            if args.eager:
+                excel_file.load_sheet_eager(sheet_name, use_columns=use_columns)
+            else:
+                excel_file.load_sheet(sheet_name, use_columns=use_columns).to_arrow()
 
 
 if __name__ == "__main__":
