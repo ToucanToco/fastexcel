b610f37b86a7e5938d5eea017b81c69d2f4645dd
Luka Peschke <luka.peschke@toucantoco.com>
2024-03-06 20:00:08 +0100

    refactor: simplify sheet loading related code (#199)
    * refactor: make ExcelSheets enum methods return FastExcelResult
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>
    
    * refactor(excelreader): common sheet loading code
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>
    
    * refactor: move IdxOrName to a types submodule
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>
    
    * refactor(rust): move python types to a python submodule
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>
    
    * refactor: provide a single load_sheet functions
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>
    
    ---------
    
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,5 +1,3 @@
 pub(crate) mod dtype;
-pub(crate) mod excelreader;
-pub(crate) mod excelsheet;
-pub(crate) use excelreader::ExcelReader;
-pub(crate) use excelsheet::ExcelSheet;
+pub(crate) mod idx_or_name;
+pub(crate) mod python;
