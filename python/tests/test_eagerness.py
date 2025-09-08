5817027154677ac158d186a33b0b373019c21807
Arnab Animesh Das <arnabanimesh@gmail.com>
2025-09-08 22:09:56 +0530

    satisfy ruff lint

--- python/tests/test_eagerness.py
+++ python/tests/test_eagerness.py
@@ -12,11 +12,11 @@ from .utils import path_for_fixture
 def test_load_sheet_eager_single_sheet() -> None:
     excel_reader = fastexcel.read_excel(path_for_fixture("fixture-single-sheet.xlsx"))
 
-    eager_pandas = excel_reader.load_sheet(0, eager = True).to_pandas()
+    eager_pandas = excel_reader.load_sheet(0, eager=True).to_pandas()
     lazy_pandas = excel_reader.load_sheet(0).to_pandas()
     pd_assert_frame_equal(eager_pandas, lazy_pandas)
 
-    eager_polars = pl.from_arrow(data=excel_reader.load_sheet(0, eager = True))
+    eager_polars = pl.from_arrow(data=excel_reader.load_sheet(0, eager=True))
     assert isinstance(eager_polars, pl.DataFrame)
     lazy_polars = excel_reader.load_sheet(0).to_polars()
     pl_assert_frame_equal(eager_polars, lazy_polars)
@@ -25,11 +25,11 @@ def test_load_sheet_eager_single_sheet() -> None:
 def test_multiple_sheets_with_unnamed_columns():
     excel_reader = fastexcel.read_excel(path_for_fixture("fixture-multi-sheet.xlsx"))
 
-    eager_pandas = excel_reader.load_sheet("With unnamed columns", eager = True).to_pandas()
+    eager_pandas = excel_reader.load_sheet("With unnamed columns", eager=True).to_pandas()
     lazy_pandas = excel_reader.load_sheet("With unnamed columns").to_pandas()
     pd_assert_frame_equal(eager_pandas, lazy_pandas)
 
-    eager_polars = pl.from_arrow(data=excel_reader.load_sheet("With unnamed columns", eager = True))
+    eager_polars = pl.from_arrow(data=excel_reader.load_sheet("With unnamed columns", eager=True))
     assert isinstance(eager_polars, pl.DataFrame)
     lazy_polars = excel_reader.load_sheet("With unnamed columns").to_polars()
     pl_assert_frame_equal(eager_polars, lazy_polars)
@@ -38,7 +38,7 @@ def test_multiple_sheets_with_unnamed_columns():
 def test_eager_with_an_ods_file_should_return_a_recordbatch() -> None:
     ods_reader = fastexcel.read_excel(path_for_fixture("dates.ods"))
 
-    record_batch = ods_reader.load_sheet(0, eager = True)
+    record_batch = ods_reader.load_sheet(0, eager=True)
     assert isinstance(record_batch, RecordBatch)
     pl_df = pl.from_arrow(record_batch)
     assert isinstance(pl_df, pl.DataFrame)
